0066a6dd6243e2ce4226c6950bfb749c8541c4ee
mrcheater <slayerx@narod.ru>
2020-12-12 16:08:49 +0300

    Init database script

--- scripts/init-database.ts
+++ scripts/init-database.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import { Client } from 'pg'
 import dotenv from 'dotenv'
+import crypto from 'crypto'
 
 dotenv.config({
   path: path.join(__dirname, '.env')
@@ -25,6 +26,8 @@ const executeStatement = async <T extends Record<string, any>>(sql: string): Pro
     await client.end()
   }
 }
+
+const md5 = (text:string) => crypto.createHmac('md5', text).digest('hex')
 
 void (async () => {
   await executeStatement(`DROP TABLE IF EXISTS ${escapeId(schemaName)}.${escapeId(usersTableName)}`)
@@ -57,42 +60,67 @@ void (async () => {
     )
   `)
 
+  const users = [
+    {
+      username: 'lykoi18',
+      avatarImageUrl: `https://scontent-frt3-2.cdninstagram.com/v/t51.2885-19/s150x150/110319607_4362037790503741_6784432024238052244_n.jpg?_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_ohc=mGtY1z-ygoQAX99auz4&tp=1&oh=0f8bc51f81092ab7aa0a7f74203b73e4&oe=5FFCFD32`
+    },
+    {
+      username: 'babich_artem',
+      avatarImageUrl: `https://user-images.githubusercontent.com/5055654/101983365-bb929a80-3c8b-11eb-8df9-64c3f16cb87f.jpg`
+    },
+    {
+      username: 'mrcheater',
+      avatarImageUrl: `https://scontent-frt3-2.cdninstagram.com/v/t51.2885-19/s150x150/45675925_523560148147503_5948854395897643008_n.jpg?_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_ohc=sjSKwWsmf34AX8jiVz_&tp=1&oh=b440ffe2f7082ade850db99780872d95&oe=5FFDDAA0`
+    }
+  ].map(
+    item => ({ ...item, passwordHash: md5(item.username) })
+  )
+  const images = [
+    `https://user-images.githubusercontent.com/5055654/101983495-5e4b1900-3c8c-11eb-8a84-44f824c586e3.jpeg`,
+    `https://user-images.githubusercontent.com/5055654/101983497-5ee3af80-3c8c-11eb-9948-0189ef4e3efa.jpeg`,
+    `https://user-images.githubusercontent.com/5055654/101983498-5f7c4600-3c8c-11eb-89fe-1366956183ba.jpeg`,
+    `https://user-images.githubusercontent.com/5055654/101983499-6014dc80-3c8c-11eb-825c-e72b99f2dd1a.jpeg`,
+    `https://user-images.githubusercontent.com/5055654/101983500-6014dc80-3c8c-11eb-91f9-7b24fc4ed8e0.jpeg`,
+    `https://user-images.githubusercontent.com/5055654/101983501-60ad7300-3c8c-11eb-9ede-71d96bdcf02f.jpeg`,
+    `https://user-images.githubusercontent.com/5055654/101983502-60ad7300-3c8c-11eb-8f9f-aa8648f0fde7.jpeg`
+  ]
+
   // Insert users
-  await executeStatement(`
-    INSERT INTO ${escapeId(schemaName)}.${escapeId(usersTableName)} (
-      ${escapeId('username')}, 
-      ${escapeId('passwordHash')}, 
-      ${escapeId('avatarImageUrl')}
-    ) VALUES (
-      ${escapeStr('mrcheater')},
-      ${escapeStr('hash(mrcheater)')},
-      ${escapeStr(`https://scontent-frt3-2.cdninstagram.com/v/t51.2885-19/s150x150/45675925_523560148147503_5948854395897643008_n.jpg?_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_ohc=sjSKwWsmf34AX8jiVz_&tp=1&oh=b440ffe2f7082ade850db99780872d95&oe=5FFDDAA0`)}
-    )
-  `)
+  for(const { username, passwordHash, avatarImageUrl } of users) {
+    await executeStatement(`
+      INSERT INTO ${escapeId(schemaName)}.${escapeId(usersTableName)} (
+        ${escapeId('username')}, 
+        ${escapeId('passwordHash')}, 
+        ${escapeId('avatarImageUrl')}
+      ) VALUES (
+        ${escapeStr(username)},
+        ${escapeStr(passwordHash)},
+        ${escapeStr(avatarImageUrl)}
+      )
+    `)
+  }
 
-  await executeStatement(`
-    INSERT INTO ${escapeId(schemaName)}.${escapeId(usersTableName)} (
-      ${escapeId('username')}, 
-      ${escapeId('passwordHash')}, 
-      ${escapeId('avatarImageUrl')}
-    ) VALUES (
-      ${escapeStr('babich_artem')},
-      ${escapeStr('hash(babich_artem)')},
-      ${escapeStr(`https://user-images.githubusercontent.com/5055654/101983365-bb929a80-3c8b-11eb-8df9-64c3f16cb87f.jpg`)}
-    )
-  `)
-
-  await executeStatement(`
-    INSERT INTO ${escapeId(schemaName)}.${escapeId(usersTableName)} (
-      ${escapeId('username')}, 
-      ${escapeId('passwordHash')}, 
-      ${escapeId('avatarImageUrl')}
-    ) VALUES (
-      ${escapeStr('lykoi18')},
-      ${escapeStr('hash(lykoi18)')},
-      ${escapeStr(`https://scontent-frt3-2.cdninstagram.com/v/t51.2885-19/s150x150/110319607_4362037790503741_6784432024238052244_n.jpg?_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_ohc=mGtY1z-ygoQAX99auz4&tp=1&oh=0f8bc51f81092ab7aa0a7f74203b73e4&oe=5FFCFD32`)}
-    )
-  `)
+  // Insert posts
+  let postIndex = 0
+  for(const { username } of users) {
+    images.sort(()=>Math.random()>0.5 ? 1 : -1)
+    for(const imageUrl of images) {
+      await executeStatement(`
+        INSERT INTO ${escapeId(schemaName)}.${escapeId(postsTableName)} (
+          ${escapeId('postId')}, 
+          ${escapeId('username')}, 
+          ${escapeId('imageUrl')}, 
+          ${escapeId('createdAt')}
+        ) VALUES (
+          ${escapeStr(`${username}_post_${postIndex++}`)},
+          ${escapeStr(username)},
+          ${escapeStr(imageUrl)},
+          ${Date.now()}
+        )
+      `)
+    }
+  }
 
   console.log(
     await executeStatement(`
